a0e25a891cf7090eb9f2e209a7f95b922dca5f60
Kailan Mao <kailanm@andrew.cmu.edu>
2024-10-24 06:35:36 -0400

    Initial Commit

---
+++ MapsAPI/MapsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsAPIApp.swift
+//  MapsAPI
+//
+//  Created by k mao on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MapsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
